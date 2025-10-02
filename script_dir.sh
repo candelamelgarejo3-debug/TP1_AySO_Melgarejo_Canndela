8037e83db6106a288c50c44ed6da09850e4a3250
Candela-Melgarejo <candelamelgarejo3@gmail.com>
2025-10-02 20:54:01 +0000

    ADD: Añadiendo script

--- script_dir.sh
+++ script_dir.sh
@@ -1,1 +1,1 @@
-mkdir -p /home/vagrant/materia1/uni1 /home/vagrant/materia1/uni2 /home/vagrant/materia1/uni3 /home/vagrant/materia2/uni1 /home/vagrant/materia2/uni2 /home/vagrant/materia2/uni3
+mkdir -p ~/{materia1,materia2}/uni{1..3}
